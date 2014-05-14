f6362efa6eaee39a26df06b66f33ea24dcaa4884
Marek Kolodziej <mkolod@gmail.com>
2014-05-14 15:15:22 -0700

    Added R package installation script to the GPL project

---
+++ server/scripts/RunRserve.R
@@ -0,0 +1,32 @@
+# This file is part of Alpine Data Labs' R Connector (henceforth " R Connector").
+# R Connector is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# R Connector is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with R Connector.  If not, see <http://www.gnu.org/licenses/>.
+
+
+# install package if it's not installed
+
+if (!"Rserve" %in% rownames(installed.packages())) {
+  install.packages('Rserve')
+}
+
+# start up Rserve
+library(Rserve)
+Rserve()
+
+# This will fail with a SOCK_ERROR if Rserve is already running
+# To make it idempotent, don't fail, just send a warning to console.
+warnUser = function(x) {print("Can't start Rserve - it's probably already running")}
+tryCatch(
+  expr = Rserve(args = "--no-save"),
+  warning = warnUser,
+  error = warnUser
+)
