af6aa10a6c85c79009971cd8e8417fc6c7efc642
Marek Kolodziej <mkolod@gmail.com>
2015-01-07 17:21:51 -0800

    Updated artifact version number

--- project/Build.scala
+++ project/Build.scala
@@ -12,7 +12,7 @@ object AlpineRConnectorBuild extends Build {
       ms filter { case (file, toPath) => !toPath.endsWith("application.conf") }
 
   lazy val sharedSettings = Defaults.defaultSettings ++ scalariformSettings ++ assemblySettings  ++ Seq(  
-    version := "0.6",
+    version := "0.7",
     organization := "com.alpine",
     scalaVersion := "2.10.3",
     fork := true,
