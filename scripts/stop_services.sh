1dbec599b441701cff0906a6406f2b356f6af6e3
Malcolm Greaves <malcolm@alpinenow.com>
2015-01-14 16:17:20 -0800

    improved logging for stop script

--- scripts/stop_services.sh
+++ scripts/stop_services.sh
@@ -4,7 +4,9 @@
 echo "Killing Rserve"
 killall Rserve
 
+echo ""
+
 # kill Java process
 echo "Killing the Scala web server"
-cat java-r-server.pid | xargs kill -9
-rm java-r-server.pid
+kill -9 `cat java-r-server.pid`
+rm -f java-r-server.pid
